2260bb5b6424cf42386a88379579ac9e820bac5e
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2024-06-10 20:43:11 +0530

    Create K-Palindrome.cpp

---
+++ Medium/K-Palindrome/K-Palindrome.cpp
@@ -0,0 +1,20 @@
+/*
+Given a string str of length n, find if the string is K-Palindrome or not. A k-palindrome string transforms into a palindrome on removing at most k characters from it.
+
+
+Example 1:
+
+Input: str = "abcdecba"
+n = 8, k = 1
+Output: 1
+Explaination: By removing 'd' or 'e' 
+we can make it a palindrome.
+
+Example 2:
+
+Input: str = "abcdefcba"
+n = 9, k = 1
+Output: 0
+Explaination: By removing a single 
+character we cannot make it a palindrome.
+*/
