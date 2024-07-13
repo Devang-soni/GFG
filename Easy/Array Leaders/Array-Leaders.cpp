42884065c8fcf537f21921294754c3b400fc0669
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2024-07-14 00:27:29 +0530

    Create Array-Leaders.cpp

---
+++ Easy/Array Leaders/Array-Leaders.cpp
@@ -0,0 +1,41 @@
+/*
+Given an array arr of n positive integers, your task is to find all the leaders in the array. An element of the array is considered a leader if it is greater than all the elements on its right side or if it is equal to the maximum element on its right side. The rightmost element is always a leader.
+
+Examples
+
+Input: n = 6, arr[] = {16,17,4,3,5,2}
+Output: 17 5 2
+Explanation: Note that there is nothing greater on the right side of 17, 5 and, 2.
+Input: n = 5, arr[] = {10,4,2,4,1}
+Output: 10 4 4 1
+Explanation: Note that both of the 4s are in output, as to be a leader an equal element is also allowed on the right. side
+Input: n = 4, arr[] = {5, 10, 20, 40} 
+Output: 40
+Explanation: When an array is sorted in increasing order, only the rightmost element is leader.
+*/
+
+class Solution {
+    // Function to find the leaders in the array.
+  public:
+    vector<int> leaders(int n, int arr[]) {
+
+  vector<int> ans;
+  
+ // Last element of an array is always a leader,
+ // push into ans array.
+ int max = arr[n - 1];
+ ans.push_back(max);
+
+  // Start checking from the end whether a number is greater
+  // than max no. from right, hence leader.
+  for (int i = n - 2; i >= 0; i--)
+    if (arr[i] > max) {
+      ans.push_back(arr[i]);
+      max = arr[i];
+    }
+
+  reverse(ans.begin(), ans.end());
+  
+  return ans;
+    }
+};
