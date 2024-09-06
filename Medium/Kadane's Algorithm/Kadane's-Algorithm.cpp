8f1a42fabac0ca977afb22bac3968ce3bbe377e2
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2024-09-06 22:18:45 +0530

    Create Kadane's-Algorithm.cpp

---
+++ Medium/Kadane's Algorithm/Kadane's-Algorithm.cpp
@@ -0,0 +1,32 @@
+/*
+Given an integer array arr[]. Find the contiguous sub-array(containing at least one number) that has the maximum sum and return its sum.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, -2, 5]
+Output: 9
+Explanation: Max subarray sum is 9 of elements (1, 2, 3, -2, 5) which is a contiguous subarray.
+Input: arr[] = [-1, -2, -3, -4]
+Output: -1
+Explanation: Max subarray sum is -1 of element (-1)
+Input: arr[] = [5, 4, 7]
+Output: 16
+Explanation: Max subarray sum is 16 of element (5, 4, 7)
+
+*/
+
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr) {
+        // code here...
+         
+        int maxi=INT_MIN;
+        int sum=0;
+        for(int i=0;i<arr.size();i++){
+            sum=max(arr[i],arr[i]+sum);
+            maxi=max(maxi,sum);
+        }
+        return maxi;
+    }
+};
