28a2d62eb2725e9c8c9dbc9d91290eb103b009e7
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2024-10-09 16:40:34 +0530

    Create Linked-List-Matrix.cpp

---
+++ Easy/Linked List Matrix/Linked-List-Matrix.cpp
@@ -0,0 +1,39 @@
+/*
+Given a Matrix mat of n*n size. Your task is constructing a 2D linked list representation of the given matrix.
+
+Input: mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+Output: 
+
+Input: mat = [[23, 28], [23, 28]]
+Output:
+
+
+*/
+
+class Solution {
+  public:
+    Node* constructLinkedMatrix(vector<vector<int>>& mat) {
+        // code Here
+        
+        int n = mat.size();
+        
+        if(n==0) return NULL;
+        vector<vector<Node*>> Nodemat(n, vector<Node*>(n,NULL));
+        
+        for(int i=0; i<n;i++){
+            for(int j=0; j<n; j++){
+                Nodemat[i][j] = new Node(mat[i][j]);
+                
+                
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(j<n-1) Nodemat[i][j] -> right = Nodemat[i][j+1];
+                if(i<n-1) Nodemat[i][j] -> down = Nodemat[i+1][j];
+            }
+        }
+        return Nodemat[0][0];
+    }
+};
