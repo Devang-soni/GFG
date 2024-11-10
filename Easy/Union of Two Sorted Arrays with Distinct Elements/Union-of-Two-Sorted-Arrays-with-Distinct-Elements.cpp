7eb6e1ca3fa7eac3d6dedb5007e35d07df6f5d31
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2024-11-10 16:40:41 +0530

    Create Union-of-Two-Sorted-Arrays-with-Distinct-Elements.cpp

---
+++ Easy/Union of Two Sorted Arrays with Distinct Elements/Union-of-Two-Sorted-Arrays-with-Distinct-Elements.cpp
@@ -0,0 +1,34 @@
+/*
+Given two sorted arrays a[] and b[], where each array contains distinct elements , the task is to return the elements in the union of the two arrays in sorted order.
+
+Union of two arrays can be defined as the set containing distinct common elements that are present in either of the arrays.
+Examples:
+
+Input: a[] = [1, 2, 3, 4, 5], b[] = [1, 2, 3, 6, 7]
+Output: 1 2 3 4 5 6 7
+Explanation: Distinct elements including both the arrays are: 1 2 3 4 5 6 7.
+Input: a[] = [2, 3, 4, 5], b[] = [1, 2, 3, 4]
+Output: 1 2 3 4 5
+Explanation: Distinct elements including both the arrays are: 1 2 3 4 5.
+Input: a[] = [1], b[] = [2]
+Output: 1 2
+Explanation: Distinct elements including both the arrays are: 1 2.
+
+*/
+
+class Solution {
+  public:
+    // a,b : the arrays
+    // Function to return a list containing the union of the two arrays.
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        vector<int> ans ;
+        set<int> st(begin(a),end(a));
+        for(int num :b){
+            st.insert(num);
+        }
+        for(auto it : st){
+            ans.push_back(it);
+        }
+        return ans;
+    }
+};
