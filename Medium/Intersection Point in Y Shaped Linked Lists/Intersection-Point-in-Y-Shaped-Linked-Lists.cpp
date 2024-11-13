c88ffc35bc0bd7266b4d7f0fa35af6dbfb5ba76b
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2024-11-13 14:33:48 +0530

    Create Intersection-Point-in-Y-Shaped-Linked-Lists.cpp

---
+++ Medium/Intersection Point in Y Shaped Linked Lists/Intersection-Point-in-Y-Shaped-Linked-Lists.cpp
@@ -0,0 +1,36 @@
+/*
+Given two singly linked lists, return the point where two linked lists intersect.
+
+Note: If the linked lists do not merge at any point, return -1.
+
+Examples:
+
+Input: Linked list 1: 4->4->4->4->4, Linked list 2: 4->4->4
+ 
+Output: 4
+Explanation: From the above image, it is clearly seen that the common part is 4->4 whose starting point is 4.
+
+Input: Linked list 1: 4->1->8->4->5, Linked List 2: 5->6->1->8->4->5
+ 
+Output: 8
+Explanation: From the above image, it is clearly seen that the common part is 8->4->5 whose starting point is 8.
+Input: Linked list 1: 1->2->3, Linked list 2: 4->5->6
+Output: -1
+Explanation: There is no common part, so there is no interaction point.
+
+*/
+
+class Solution {
+  public:
+    int intersectPoint(Node* head1, Node* head2) {
+        Node* t1 = head1, *t2 = head2;
+        while ( head1 && head2 ){
+            if ( head1 == head2 ) return head1->data;
+            head1 = head1->next;
+            head2 = head2->next;
+            if ( head1 == NULL && head2 == NULL ) return -1;
+            if ( head1 == NULL ) head1 = t2;
+            if ( head2 == NULL ) head2 = t1;
+        } return -1;
+    }
+};
